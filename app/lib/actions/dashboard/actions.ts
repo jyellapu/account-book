ab1d7eea26a09904392476f119793530d9f97c45
Janakiram Yellapu <janakiramyellapu@gmail.com>
2024-03-08 17:31:33 +0530

    Added server actions for dashboard cards.

---
+++ app/lib/actions/dashboard/actions.ts
@@ -0,0 +1,68 @@
+'use server'
+
+import { AccountStatusType } from "@prisma/client"
+import { prisma } from "../../db"
+
+export async function getCardData(bookId: number) {
+    const totalCustomersPromise = prisma.customer.aggregate({
+        _count: {
+            id: true,
+        },
+        where: {
+            bookId: bookId
+        }
+    })
+
+    const totalOpenAccountsPromise = prisma.account.aggregate({
+        _count: {
+            id: true,
+        },
+        where: {
+            status: AccountStatusType.OPEN,
+            customer: {
+                bookId: bookId
+            },
+        }
+    })
+
+    const totalAmountGivenPromise = prisma.account.aggregate({
+        _sum: {
+            amount: true
+        },
+        where: {
+            status: AccountStatusType.OPEN,
+            customer: {
+                bookId: bookId
+            }
+        }
+    })
+
+    const totalAmountCollectedPromise = prisma.transaction.aggregate({
+        _sum: {
+            amount: true
+        },
+        where: {
+            account: {
+                status: AccountStatusType.OPEN,
+                customer: {
+                    bookId: bookId
+                }
+            }
+        }
+    })
+    const data = await Promise.all([
+        totalCustomersPromise,
+        totalOpenAccountsPromise,
+        totalAmountGivenPromise,
+        totalAmountCollectedPromise
+    ])
+
+    console.log(data)
+    const totalCustomers = data[0]._count.id
+    const totalOpenAccounts = data[1]._count.id
+    const totalAmountGiven = Number(data[2]._sum.amount)
+    const totalAmountCollected = Number(data[3]._sum.amount)
+    const totalAmountPending = totalAmountGiven - totalAmountCollected
+
+    return { totalCustomers, totalOpenAccounts, totalAmountCollected, totalAmountPending }
+}
