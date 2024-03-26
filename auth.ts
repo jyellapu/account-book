ee77392a69d62f416061239de33872fdcb5e0840
Janakiram Yellapu <janakiramyellapu@gmail.com>
2024-03-26 22:13:33 +0530

    Removed credentials provider and added google provider.

--- auth.ts
+++ auth.ts
@@ -1,54 +1,13 @@
 import NextAuth from 'next-auth';
-import Credentials from 'next-auth/providers/credentials';
+import GoogleProvider from "next-auth/providers/google";
 import { authConfig } from './auth.config';
-import { z } from 'zod';
-import bcrypt from 'bcrypt';
-import { prisma } from './app/lib/db';
-import { StaffType } from '@prisma/client';
 
-async function getStaff(mobileNumber: string) {
-  try {
-    const staff = await prisma.staff.findFirst({
-      where: {
-        mobileNumber: mobileNumber
-      },
-      select: {
-        id: true,
-        firstName: true,
-        lastName: true,
-        mobileNumber: true,
-        password: true,
-        type: true
-      }
-    })
-    return staff;
-  } catch (error) {
-    console.error('Failed to fetch user:', error);
-    throw new Error('Failed to fetch user.');
-  }
-}
-
-export const { auth, signIn, signOut } = NextAuth({
+export const { auth, signIn, signOut, handlers: { GET, POST } } = NextAuth({
   ...authConfig,
   providers: [
-    Credentials({
-      async authorize(credentials) {
-        const parsedCredentials = z
-          .object({ mobileNumber: z.string(), password: z.string().min(6) })
-          .safeParse(credentials);
-
-        if (parsedCredentials.success) {
-          const { mobileNumber, password } = parsedCredentials.data;
-          const staff = await getStaff(mobileNumber);
-          if (!staff || ![StaffType.ADMIN, StaffType.EMPLOYEE].includes(staff.type)) return null;
-          const passwordsMatch = await bcrypt.compare(password, staff.password);
-          if (passwordsMatch) {
-            return { id: staff.id.toString(), name: staff.firstName + " " + staff.lastName, email: staff.mobileNumber.toString() }
-          }
-        }
-
-        return null;
-      },
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
 });
