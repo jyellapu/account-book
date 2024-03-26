4f5d7244f6bda54a0359f98f39667229ba0640ab
Janakiram Yellapu <janakiramyellapu@gmail.com>
2024-03-26 22:15:08 +0530

    On sign in creating new creating new account for the user.

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,2 +1,4 @@
 export const DEFAULT_DUE_DATE_INTERVAL_IN_DAYS = 90;
 export const ITEMS_PER_PAGE = 6;
+export const EMPTY_USER_NAME = "NO NAME";
+export const DEFAULT_NEW_ACCOUNT_BOOK_NAME = "New account book";
--- auth.config.ts
+++ auth.config.ts
@@ -1,4 +1,7 @@
+import { Role } from '@prisma/client';
 import type { NextAuthConfig } from 'next-auth';
+import { DEFAULT_NEW_ACCOUNT_BOOK_NAME, EMPTY_USER_NAME } from '@/app/lib/constants';
+import { prisma } from '@/app/lib/db';
 
 export const authConfig = {
   pages: {
@@ -17,6 +20,33 @@ export const authConfig = {
       }
       return true;
     },
+    async signIn({ user, account, profile }) {
+      if (account?.provider == "google" && user.email) {
+        const existingUser = await prisma.staff.findFirst({
+          where: {
+            email: user.email
+          }
+        })
+        if (!existingUser) {
+          await prisma.staff.create({
+            data: {
+              name: user.name || EMPTY_USER_NAME,
+              email: user.email,
+              image: user.image,
+              books: {
+                create: [
+                  {
+                    book: { create: { name: DEFAULT_NEW_ACCOUNT_BOOK_NAME } },
+                    role: Role.OWNER
+                  },
+                ],
+              }
+            }
+          })
+        }
+      }
+      return true;
+    }
   },
   providers: [], // Add providers with an empty array for now
 } satisfies NextAuthConfig;
